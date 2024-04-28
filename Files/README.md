2698d04995bd2055424e99d58ab633e79e7c3a25
Erica Balderas <ericab40033@hotmail.com>
2024-04-28 14:57:07 -0600

    adding table

---
+++ Files/README.md
@@ -0,0 +1,24 @@
+
+# Testing
+
+## Description
+hello
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+Steps on how to install
+
+## Usage
+generator
+
+## License
+// Add license badge and explanation of license
+
+## Questions
+For additional questions, contact [GitHub](https://github.com/mistaken40033) or email: ericab40033@hotmail.com
+    
--- Files/index.js
+++ Files/index.js
@@ -1,13 +1,66 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+async function generateREADME() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your project:'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Instructions on how to install:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is this project used for?'
+        },
+        // Add more prompts for license, contributing guidelines, tests, etc.
+    ]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    const { title, description, installation, usage /*, other answers */ } = answers;
 
-// TODO: Create a function to initialize app
-function init() {}
+    // Generate Markdown content based on user input
+    const markdownContent = `
+# ${title}
 
-// Function call to initialize app
-init();
+## Description
+${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+// Add license badge and explanation of license
+
+## Questions
+For additional questions, contact [GitHub](https://github.com/mistaken40033) or email: ericab40033@hotmail.com
+    `;
+
+    // Write the generated Markdown content to README.md file
+    fs.writeFileSync('README.md', markdownContent);
+
+    console.log('README.md generated successfully.');
+}
+
+// Invoke the function to generate README.md
+generateREADME().catch(error => {
+    console.error('An error occurred:', error);
+});
